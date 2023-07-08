7d984954eeef6ca8671268a18b694914bf22ffde
vera0708 <verasdee@gmail.com>
2023-07-08 21:28:40 +0700

    feat: try to make Color. jsx

--- src/Components/ColorList/Color/Color.jsx
+++ src/Components/ColorList/Color/Color.jsx
@@ -1,7 +1,23 @@
+import { useRef } from 'react';
+// import { useEffect } from 'react';
+import style from './Color.module.scss';
+
 export const Color = ({ color, check }) => {
-    console.log(check);
+    console.log('check:  ', check);
+    const activeElement = useRef(0);
+
+    const checkedCircle = () => {
+        console.log(activeElement.current)
+        // activeElement.current = activeElement.current.checked();
+
+    };
 
     return (
-        <li>{color}</li>
+        <button
+            // className={{check} => cn(style.color && style.colorCheck)}
+            className={style.color}
+            style={{ backgroundColor: `${color}` }}
+            onClick={checkedCircle}
+        ></button>
     )
-}
+} 
