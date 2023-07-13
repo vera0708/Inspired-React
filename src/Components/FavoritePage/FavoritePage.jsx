20b6d6940c3ea9514baf6e5edfb0f82d0a9db6c1
vera0708 <verasdee@gmail.com>
2023-07-13 15:18:05 +0700

    feat: begin pagination

--- src/Components/FavoritePage/FavoritePage.jsx
+++ src/Components/FavoritePage/FavoritePage.jsx
@@ -1,10 +1,19 @@
 import { useDispatch, useSelector } from "react-redux"
 import { Goods } from "../Goods/Goods"
 import { useEffect } from "react";
-import { fetchCategory } from "../../features/goodsSlice";
+import { fetchCategory, setPage } from "../../features/goodsSlice";
+import { useLocation } from "react-router-dom";
 
 export const FavoritePage = () => {
     const dispatch = useDispatch();
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const pageURL = searchParams.get('page');
+    console.log('pageURL: ', pageURL);
+
+    useEffect(() => {
+        dispatch(setPage(pageURL))
+    }, [pageURL, dispatch])
 
     const favorites = useSelector(state => state.favorites);
 
